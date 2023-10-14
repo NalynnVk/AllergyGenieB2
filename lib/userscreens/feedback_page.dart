d3bb9c2044b3879bbeb89e466415bbced56a6f49
Nalynn Vk <134136410+NalynnVk@users.noreply.github.com>
2023-10-14 12:13:14 +0800

    Before Firebase

--- lib/userscreens/feedback_page.dart
+++ lib/userscreens/feedback_page.dart
@@ -1,16 +1,6 @@
 import 'package:calendar/userscreens/profile_page.dart';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FeedbackPage(),
-    );
-  }
-}
-
 class FeedbackPage extends StatefulWidget {
   @override
   _FeedbackPageState createState() => _FeedbackPageState();
